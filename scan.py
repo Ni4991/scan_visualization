d9851aa214dfadca3bcc27aff3a13c90755253c9
Ni Luo <nina@Nis-MacBook-Pro.local>
2018-03-22 16:29:10 -0700

    scan v

--- scan.py
+++ scan.py
@@ -13,11 +13,24 @@ def run():
     
     pcap_data = pd.read_csv('dump.csv', index_col='No.')
 
-    df = pcap_data.groupby('Protocol')['Protocol'].count()
-
+    df = pcap_data.groupby('Source')['Source'].count().reset_index(name='count') \
+                             .sort_values(['count'], ascending=False)
     df.plot(kind='bar')
 
+
+    x = []
+    label_x = []
+    for index, row in df.iterrows():
+       x.append(index)
+       label_x.append(row['Source'])
+
+
+    plt.title("IP Traffic by packages")
+    plt.subplots_adjust(bottom=0.2)
+    plt.xticks(x, label_x)
     plt.show()
+
+
 
 
 if __name__ == '__main__':
